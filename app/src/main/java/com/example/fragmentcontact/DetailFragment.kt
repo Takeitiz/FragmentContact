ef5300e699decdc2b671dca7a3fa56db3ada91a6
Takeitiz <99989483+Takeitiz@users.noreply.github.com>
2023-12-02 22:02:46 +0700

    Update DetailFragment.kt

--- app/src/main/java/com/example/fragmentcontact/DetailFragment.kt
+++ app/src/main/java/com/example/fragmentcontact/DetailFragment.kt
@@ -9,7 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 
 class DetailFragment : Fragment() {
-    lateinit var ID: TextView
+    lateinit var Id: TextView
     lateinit var NAME: TextView
     lateinit var NUMBER: TextView
     lateinit var EMAIL: TextView
@@ -20,20 +20,12 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_detail, container, false)
-
-        ID = view.findViewById(R.id.idTextView)
-        NAME = view.findViewById(R.id.nameTextView)
-        NUMBER = view.findViewById(R.id.numberTextView)
-        EMAIL = view.findViewById(R.id.emailTextView)
-        THUMB = view.findViewById(R.id.thumbImageView)
-
-        return view
+        return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        ID = view.findViewById(R.id.idTextView)
+        Id = view.findViewById(R.id.idTextView)
         NAME = view.findViewById(R.id.nameTextView)
         NUMBER = view.findViewById(R.id.numberTextView)
         EMAIL = view.findViewById(R.id.emailTextView)
@@ -42,7 +34,7 @@ class DetailFragment : Fragment() {
 
     fun UpdateContent(thumb: Int, id: String, name: String, number: String, email: String) {
         THUMB.setImageResource(thumb)
-        ID.text = "ID: " + id
+        Id.text = "ID: " + id
         NAME.text = "Tên: " + name
         NUMBER.text = "Phone number: " + number
         EMAIL.text = "Email: " + email
